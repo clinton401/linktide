09a1287ab585f5f9277c4589628631801f831d9c
clinton <clintonphillips464@gmail.com>
2025-05-20 21:00:29 +0100

    added loaders

---
+++ app/(protected)/loading.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { LoaderParentComponent } from "@/components/loader-parent-component";
+
+const ProtectedLoadingPage: FC = () => {
+  return <LoaderParentComponent />;
+};
+
+export default ProtectedLoadingPage;
---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { LoaderParentComponent } from "@/components/loader-parent-component";
+
+const LoadingPage: FC = () => {
+  return <LoaderParentComponent />;
+};
+
+export default LoadingPage;
